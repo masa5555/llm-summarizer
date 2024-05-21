ab1641bd97670f97f5e55d20d1724883eef91145
masa5555 <masa57887@gmail.com>
2024-05-21 23:00:51 +0900

    Add: Web Summary

--- a.py
+++ a.py
@@ -5,36 +5,57 @@ import requests
 import datetime
 import os
 import re
+import textwrap
 from dotenv import load_dotenv
 
 load_dotenv()
 
 url = input("Enter URL: ")
-response = requests.get(url)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 title = soup.title.text.replace(r'/', "|", 100)
 
+if title == "403 Forbidden":
+    print("403 Forbidden")
+    exit()
+
 chunked_transcript = []
-chunk = ""
+CHUNK_WORD_LIMIT = 1000
 
 if url.find("youtube.com") != -1:
     videoId = url.split("v=")[1]
     transcript = YouTubeTranscriptApi.get_transcript(videoId)
 
+    chunk = ""
     for i in transcript:
         chunk += i['text'] + " "
-        if len(chunk.split(" ")) > 1000:
+        if len(chunk.split(" ")) > CHUNK_WORD_LIMIT:
             chunked_transcript.append(chunk)
             chunk = ""
     chunked_transcript.append(chunk)
-
+else:
+    text = soup.get_text()
+    text = textwrap.dedent(text)
+    word_list = text.split()
+    chunk = ""
+    for word in word_list:
+        chunk += word + " "
+        if len(chunk.split(" ")) > CHUNK_WORD_LIMIT:
+            chunked_transcript.append(chunk)
+            chunk = ""
+    chunked_transcript.append(chunk)
 
 client = Anthropic(
     api_key=os.getenv("ANTHROPIC_API_KEY"),
 )
 
 systemPrompt = """
-あなたは日本語で要約するエージェントです。与えられた文章がどんな言語でも日本語で要約を出力しなさい。出力フォーマットはネストされた箇条書きで構造化し、それ以外の前後のつなぎ無しで応答して下さい。出力例```- XXX\n   - XXXの詳細1\n   - XXXの詳細2\n - YYY\n```
+あなたは日本語で要約するエージェントです。与えられた文章がどんな言語でも日本語で要約を出力しなさい。出力フォーマットはマークダウンで主にネストされた箇条書きで文書として構造化して応答しなさい。参考文献をURLで引用している場合は箇条書き要素の文末に追加しなさい。出力例```# 要約タイトル\n- XXX\n   - XXXの詳細1\n   - XXXの詳細2\n - YYY\n```
 """
 
 def requestLLM(prompt):
@@ -49,17 +70,20 @@ def requestLLM(prompt):
         system=systemPrompt,
         model="claude-3-haiku-20240307",
     )
+    print("------------------Usage--------------", message.usage)
     return message.content[0].text
 
-with open("{} {}.txt".format(title, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), "w") as f:
-    for chunk in chunked_transcript:
-        ansText = requestLLM(chunk)
+with open("{} {}.md".format(title, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), "w") as f:
+    for script in chunked_transcript:
+        ansText = requestLLM(script)
         # ひらがなが含まれているかどうかで判定
+        print("re.match(r'[ぁ-ん]', ansText)----------", re.match(r'[ぁ-ん]', ansText))
         if not re.match(r'[ぁ-ん]', ansText):
             message = requestLLM("以下の文章を日本語に変換してください\n```" + ansText + "\n```" )
 
         f.write(ansText + "\n\n")
-        print(chunk, ansText)
+        print(script)
+        print(ansText)
     f.close()
 
 
