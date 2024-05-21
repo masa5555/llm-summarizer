5d164931d2dabd6661dc69519e379f4ca849b97d
masa5555 <masa57887@gmail.com>
2024-05-21 20:10:20 +0900

    fix: 日本語を強制

--- a.py
+++ a.py
@@ -2,53 +2,65 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from anthropic import Anthropic
 from bs4 import BeautifulSoup
 import requests
+import datetime
+import os
+import re
+from dotenv import load_dotenv
 
-videoId = input("Enter the video ID: ")
+load_dotenv()
 
-url = "https://www.youtube.com/watch?v={}".format(videoId)
+url = input("Enter URL: ")
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.title.text)
-title = soup.title.text
-
-transcript = YouTubeTranscriptApi.get_transcript(videoId)
-print(transcript)
+title = soup.title.text.replace(r'/', "|", 100)
 
 chunked_transcript = []
 chunk = ""
-for i in transcript:
-  chunk += i['text'] + " "
-  if len(chunk.split(" ")) > 1000:
+
+if url.find("youtube.com") != -1:
+    videoId = url.split("v=")[1]
+    transcript = YouTubeTranscriptApi.get_transcript(videoId)
+
+    for i in transcript:
+        chunk += i['text'] + " "
+        if len(chunk.split(" ")) > 1000:
+            chunked_transcript.append(chunk)
+            chunk = ""
     chunked_transcript.append(chunk)
-    chunk = ""
-chunked_transcript.append(chunk)
+
 
 client = Anthropic(
-    # This is the default and can be omitted
-    api_key="xxxxx",
+    api_key=os.getenv("ANTHROPIC_API_KEY"),
 )
 
-translated = []
+systemPrompt = """
+あなたは日本語で要約するエージェントです。与えられた文章がどんな言語でも日本語で要約を出力しなさい。出力フォーマットはネストされた箇条書きで構造化し、それ以外の前後のつなぎ無しで応答して下さい。出力例```- XXX\n   - XXXの詳細1\n   - XXXの詳細2\n - YYY\n```
+"""
 
-for chunk in chunked_transcript:
+def requestLLM(prompt):
     message = client.messages.create(
         max_tokens=1024,
         messages=[
             {
                 "role": "user",
-                "content": chunk,
+                "content": prompt,
             },
         ],
-        retry_on_rate_limit=True,
-        system="あなたは要約エージェントです。与えられた文章について日本語で要約してください。出力フォーマットはネストされた箇条書きで構造化し、それ以外の前後のつなぎ無しで応答して下さい。出力例```- 要点1\n   - 要点1-詳細A\n   - 要点1-詳細B\n - 要点2\n```",
+        system=systemPrompt,
         model="claude-3-haiku-20240307",
     )
-    translated.append(message.content[0].text)
-    print(chunk, message.content[0].text)
+    return message.content[0].text
 
-with open("{}.txt".format(title), "w") as f:
-    for chunk in translated:
-        f.write(chunk + "\n\n")
+with open("{} {}.txt".format(title, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), "w") as f:
+    for chunk in chunked_transcript:
+        ansText = requestLLM(chunk)
+        # ひらがなが含まれているかどうかで判定
+        if not re.match(r'[ぁ-ん]', ansText):
+            message = requestLLM("以下の文章を日本語に変換してください\n```" + ansText + "\n```" )
+
+        f.write(ansText + "\n\n")
+        print(chunk, ansText)
+    f.close()
 
 
 
