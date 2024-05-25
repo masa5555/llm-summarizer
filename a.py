8f3e668bc37cd9db4e199538c3e1da40085e80c8
masa5555 <masa57887@gmail.com>
2024-05-25 22:58:59 +0900

    notionDBに挿入できるようにした

--- a.py
+++ a.py
@@ -7,6 +7,7 @@ import os
 import re
 import textwrap
 from dotenv import load_dotenv
+from notion_client import Client as notionClient
 
 load_dotenv()
 
@@ -54,8 +55,14 @@ client = Anthropic(
     api_key=os.getenv("ANTHROPIC_API_KEY"),
 )
 
+notion = notionClient(auth=os.environ["NOTION_API_KEY"])
+
 systemPrompt = """
-あなたは日本語で要約するエージェントです。与えられた文章がどんな言語でも日本語で要約を出力しなさい。出力フォーマットはマークダウンで主にネストされた箇条書きで文書として構造化して応答しなさい。参考文献をURLで引用している場合は箇条書き要素の文末に追加しなさい。出力例```# 要約タイトル\n- XXX\n   - XXXの詳細1\n   - XXXの詳細2\n - YYY\n```
+・あなたは日本語で要約するエージェントです。
+・与えられた文章がどんな言語でも日本語で要約を出力しなさい。
+　・文書として情報量のない言葉は要約の中に絶対に入れないでください。
+　・参考文献をURLで引用している場合は箇条書き要素の文末に追加しなさい。
+・出力フォーマットはマークダウンで主にネストされた箇条書きで文書として構造化して応答しなさい。出力例```# 要約した内容が一言でわかるタイトル\n- XXX\n   - XXXの詳細1\n   - XXXの詳細2\n - YYY\n```
 """
 
 def requestLLM(prompt):
@@ -73,19 +80,119 @@ def requestLLM(prompt):
     print("------------------Usage--------------", message.usage)
     return message.content[0].text
 
-with open("{} {}.md".format(title, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), "w") as f:
+def createNotionPage(title, url):
+    response = notion.pages.create(
+        parent={"database_id": os.getenv("NOTION_DATABASE_ID")},
+        properties={
+            "Title": {
+                "title": [
+                    {
+                        "text": {
+                            "content": title,
+                            "link": {
+                                "url": url
+                            }
+                        }
+                    }
+                ]
+            },
+            "Summary": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": ""
+                        }
+                    }
+                ]
+            }
+        }
+    )
+    pageId = response["id"]
+    print("pageId", pageId)
+    notion.blocks.children.append(
+        block_id=pageId,
+        children=[
+            {
+                "object": "block",
+                "type": "table_of_contents",
+                "table_of_contents": {}
+            }
+        ]
+    )
+    return pageId
+
+def createNotionBlock(type, text):
+    block = {
+        "object": "block",
+        "type": type,
+        type: {
+            "rich_text": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": text
+                    }
+                }
+            ]
+        }
+    }
+    return block
+
+def updatePageSummary(pageId, summary, tags):
+    tagBlocks = [{"name": tag} for tag in tags]
+    notion.pages.update(
+        page_id=pageId,
+        properties={
+            "Summary": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": summary
+                        }
+                    }
+                ]
+            },
+            "Tags": {
+                "multi_select": tagBlocks
+            }
+        }
+    )
+    return
+
+with open("summaries/{} {}.md".format(title, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), "w") as f:
+    f.write(url + "\n\n")
+    pageId = createNotionPage(title, url)
+    ansTextSum = ""
     for script in chunked_transcript:
         ansText = requestLLM(script)
+        ansTextSum += ansText
         # ひらがなが含まれているかどうかで判定
         print("re.match(r'[ぁ-ん]', ansText)----------", re.match(r'[ぁ-ん]', ansText))
         if not re.match(r'[ぁ-ん]', ansText):
             message = requestLLM("以下の文章を日本語に変換してください\n```" + ansText + "\n```" )
 
         f.write(ansText + "\n\n")
-        print(script)
-        print(ansText)
+        ansTextLines = ansText.split("\n")
+        blocks = []
+        for line in ansTextLines:
+            if line.startswith("# "):
+                blocks.append(createNotionBlock("heading_1", line.replace("# ", "")))
+            elif line.startswith("## "):
+                blocks.append(createNotionBlock("heading_2", line.replace("## ", "")))
+            elif line.startswith("### "):
+                blocks.append(createNotionBlock("heading_3", line.replace("### ", "")))
+            else:
+                blocks.append(createNotionBlock("bulleted_list_item", line))
+        # print(blocks)
+
+        notion.blocks.children.append(
+            block_id=pageId,
+            children=blocks
+        )
+    summary = requestLLM("以下の文章を1文かつ100文字以内で要約し、その要約1文のみ短答してください。それ以外の文字は出力に含めないでください。\n```" + ansTextSum + "\n```" )
+    tagsText = requestLLM("以下の文章から主要なキーワードを重要度が高い順に5個抜き出し、カンマ区切りでキーワードのリストだけを出力して下さい\n```" + ansTextSum + "\n```" )
+    tags = []
+    if len(tagsText.split(",")) > 0:
+        tags = tagsText.split(",")[:5]
+    updatePageSummary(pageId, summary, tags)
     f.close()
-
-
-
-
